8ddb2bfa07df9c42d0fc0f117ddd77903ebe351f
waleed <waleed@waleeds-MacBook-Pro.local>
2026-08-17 14:55:48 +0500

    change url

--- src/data/site.ts
+++ src/data/site.ts
@@ -16,7 +16,7 @@ export const siteConfig: SiteConfig = {
 
   location: "Lahore, Pakistan",
 
-  url: "https://waleed-portfolio-self.vercel.app",
+  url: "https://waleed-bin-khalid.vercel.app",
 
   socialLinks: {
     github: "https://github.com/WaleedBinKhalid123",
